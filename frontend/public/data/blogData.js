a709de539269ec76fd07a89fa8d6c1c4f9c4a686
epchilders1 <echilders2004@gmail.com>
2024-05-26 17:07:54 -0600

    gkgkg

--- frontend/public/data/blogData.js
+++ frontend/public/data/blogData.js
@@ -1,5 +1,11 @@
 const data = [
-  
+  {
+    "id": "8ddf249f-1cd6-4ef9-a098-db4d46cad4af",
+    "desc": "123123",
+    "images": [
+      "EvanChilders-rxaAFxH2.jpg"
+    ]
+  }
 ];
 
 const getPosts = () => {
--- frontend/public/data/galleryImage.js
+++ frontend/public/data/galleryImage.js
@@ -1,5 +1,8 @@
 const data = [
-  
+  {
+    "id": "3df8b587-9c0c-4c6b-8e56-86628268b210",
+    "cover": "NebulaNavigators-JYBlS6Ek.png"
+  }
 ];
 
 const getProducts = () => {
